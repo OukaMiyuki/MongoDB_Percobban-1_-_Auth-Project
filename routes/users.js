235605428488938db0b417c2f792403398cc4600
OukaMiyuki <asukaloid.blog@gmail.com>
2020-07-24 15:47:30 +0700

    Penambahan implementasi hashing pasword menggunakan bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const {User, validate} = require('../models/user');
 const mongoose = require('mongoose');
@@ -13,7 +14,11 @@ router.post('/', async (req, res) => {
         if(user) res.status(400).send('Email or user has already been registered!');
 
         user = new User(_.pick(req.body, ['name', 'email', 'password']));
-        user = await user.save();
+
+        const salt = await bcrypt.genSalt(10);//create salt with default character 10, you can feel this this whatever value you wanna store for example 20
+        user.password = await bcrypt.hash(user.password, salt);//hashing the password
+        await user.save();
+
         res.send(_.pick(user, ['_id','name', 'email']));
     } catch(ex){
         console.log('There\'s an error ', ex.message);
