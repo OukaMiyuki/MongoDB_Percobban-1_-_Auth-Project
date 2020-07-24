025788f235b3e42d569cce4bef6b66365593de29
OukaMiyuki <asukaloid.blog@gmail.com>
2020-07-24 15:00:26 +0700

    Penambahan implementasi lodash

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const {User, validate} = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
@@ -11,16 +12,12 @@ router.post('/', async (req, res) => {
         let user = await User.findOne( { email: req.body.email } );
         if(user) res.status(400).send('Email or user has already been registered!');
 
-        user = new User(
-            { 
-                name: req.body.name,
-                email: req.body.email,
-                password: req.body.password 
-            });
+        user = new User(_.pick(req.body, ['name', 'email', 'password']));
         user = await user.save();
-        res.send(user);
+        res.send(_.pick(user, ['_id','name', 'email']));
     } catch(ex){
         console.log('There\'s an error ', ex.message);
+        //res.status(400).send('An error occured', ex.message);
     }
 });
 
